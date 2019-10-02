3632bff35f2003169bd421678e79043e86d6c9db
Subin <subinsamrat96@gmail.com>
2019-10-02 22:33:13 +0530

    optimized the similar components and adding onchange / delete event handlers

--- src/App.js
+++ src/App.js
@@ -1,90 +1,159 @@
-import React from 'react';
-import './App.css';
-import Person from './Person/Person';
-import UserOutput from './UserOutput/userOutput';
-import UserInput from './UserInput/userInput';
+import React from "react";
+import "./App.css";
+import Person from "./Person/Person";
+// import UserOutput from './UserOutput/userOutput';
+// import UserInput from './UserInput/userInput';
 
 class App extends React.Component {
+  state = {
+    persons: [
+      { id: 'fgh', name: "Subin", age: 23 },
+      { id: 'tgh', name: "Samrat", age: 24 },
+      { id: 'ygh', name: "SubinEdge", age: 20 }
+    ],
+    showPersons: false
+  };
+
   // state = {
-  //   persons : [
-  //     {name:'Subin', age:23},
-  //     {name:'Samrat', age:24},
-  //     {name:'SubinEdge', age:20}
-  //   ]
+  //   username : 'maximilian'
   // }
 
-  state = {
-    username : 'maximilian'
-  }
-
-
-  userNameHandler = (event) => {
+  userNameHandler = event => {
     this.setState({
-      username : event.target.value
+      username: event.target.value
     });
+  };
+
+  // switchNameHandler = newName => {
+  //   // console.log('was clicked!!!');
+
+  //   // do not mutate state
+
+  //   // this.state.persons[0].name = "Sudheesh";
+
+  //   this.setState({
+  //     persons: [
+  //       { name: newName, age: 27 },
+  //       { name: "Samrat", age: 24 },
+  //       { name: "Subinedge", age: 47 }
+  //     ]
+  //   });
+  // };
+
+
+
+
+  nameChangehandler = (event, id) => {
+
+    // the name will changed for the input changed for that component.. so we have to identify the id of that particular component and add 
+
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {...this.state.persons[personIndex]};
+
+    person.name = event.target.value;
+
+    // after targeting the input value by event, then update the array.
+
+    const persons = [...this.state.persons];
+
+    persons[personIndex] = person;
+
+    this.setState({persons:persons});
   }
 
-  switchNameHandler = (newName) => {
-    // console.log('was clicked!!!');
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons : !doesShow}) 
+  };
 
-    // do not mutate state
 
-    // this.state.persons[0].name = "Sudheesh"; 
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons;
 
-    this.setState({
-      persons : [
-        {name:newName, age: 27},
-        {name:'Samrat', age: 24},
-        {name:'Subinedge', age: 47}
-      ]
-    })
+    // above method is not a good practice, becoz.. we are trying to mutate the original state. In JS, objects, arrays are reference types. So assigning to another variable is nothing but the reference object is pointing to another variable.
+
+    // So , its better to duplicate the array by spread operator and mutate that.
+
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons:persons})
   }
-
-
-  nameChangehandler = (event) => {
-    this.setState({
-      persons : [
-        {name:'Subin', age: 27},
-        {name:event.target.value, age: 24},
-        {name:'Subinedge', age: 47}
-      ]
-    })
-  }
-
-
-
 
   render() {
+
+    let persons = null;
+
+    if(this.state.showPersons) {
+      persons = (
+          <div>
+
+          {/* rather than creating each similar component, we can map those similar components into 1, so that when we add an extra component it wont break and also no need to pass the state data manually to each component. This will be useful in blog posts. */}
+
+        {/* IMPORTANT */}
+
+
+             {this.state.persons.map( (person, index) => {
+              return <Person
+              click = {this.deletePersonHandler.bind(this, index)}
+              name={person.name}
+              age={person.age}
+              key ={person.id}
+              changed ={(event) => this.nameChangehandler(event, person.id)}/>
+            })}
+
+
+
+
+            {/* <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+            >
+              My dream is to become a Web developer
+            </Person>
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              changed={this.nameChangehandler}
+              click={this.switchNameHandler.bind(this, "Sudhakaran")}
+            />
+            <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age}
+            /> */}
+          </div>
+      )
+      
+    }
+
     return (
       <div className="App">
         <header className="App-header">
-          <h1>
-            REACT IS WORKING !!
-          </h1>
-          {/* <button onClick={this.switchNameHandler.bind(this, 'Bindhu')}>Switch Name</button>
-          <Person 
-            name={this.state.persons[0].name} 
-            age={this.state.persons[0].age}>My dream is to become a Web developer</Person>
-          <Person 
-            name={this.state.persons[1].name} 
-            age={this.state.persons[1].age}
-            changed = {this.nameChangehandler}
-            click = {this.switchNameHandler.bind(this, 'Sudhakaran')}/>
-          <Person 
-            name={this.state.persons[2].name} 
-            age={this.state.persons[2].age}/> */}
+          <h1>REACT IS WORKING !!</h1>
+
+          {/* pass arguments while calling function */}
+          {/* <button onClick={this.switchNameHandler.bind(this, 'Bindhu')}>Switch Name</button> */}
+          <button onClick={this.togglePersonsHandler}>Toggle Name</button>
+
+          {persons}
+
+          <br/><br/><br/>
+
+          <hr/>
+
           
-          <UserInput usernameHandle = {this.userNameHandler} userName = {this.state.username}/>
+
+          {/* <UserInput usernameHandle = {this.userNameHandler} userName = {this.state.username}/>
           <UserOutput userName = "subin"/>
           <UserOutput userName = {this.state.username} />
-          <UserOutput userName = {this.state.username}/>
-
+          <UserOutput userName = {this.state.username}/> */}
         </header>
       </div>
     );
   }
-
-  
 }
+
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,9 +5,9 @@ const person = (props) => {
   return (
     <div className = "Person">
       <p onClick={props.click}>I am a {props.name} component and my age is {props.age}</p>
-      {/* {props.children} */}
+      <p>{props.children}</p>
 
-      <input type="text" onChange = {props.changed} value={props.name}/>
+      <input type="text" onChange = {props.changed} defaultValue={props.name}/>
     </div>
   );
 }
